a031b5c7b7e319c1c1813322c1fa61e874a8889f
Jefferson David <jeffersonchile2001@gmail.com>
2022-09-26 16:29:41 -0500

    simple-snake

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const PIEDRA = 1;
-const PAPEL = 2;
-const TIJERA = 3;
+const scorePc = document.querySelector(".score-pc");
+const scorePlayer = document.querySelector(".score-player");
+
+const FIRE = 1;
+const WATER = 2;
+const EARTH = 3;
 let conclusion = 0;
 
 const opcionPc = (min, max) => {
@@ -8,18 +11,18 @@ const opcionPc = (min, max) => {
 };
 
 const play = (player) => {
-  const pcPiedra = document.getElementById("cardPc-piedra");
-  const pcPapel = document.getElementById("cardPc-papel");
-  const pcTijera = document.getElementById("cardPc-tijera");
+  const pcWater = document.getElementById("cardPc-water");
+  const pcFire = document.getElementById("cardPc-fire");
+  const pcEarth = document.getElementById("cardPc-earth");
   const pc = opcionPc(1, 3);
 
   if (pc == player) {
     console.log("EMPATE ");
     conclusion = 0;
   } else if (
-    (player == PIEDRA && pc == TIJERA) ||
-    (player == PAPEL && pc == PIEDRA) ||
-    (player == TIJERA && pc == PAPEL)
+    (player == FIRE && pc == EARTH) ||
+    (player == WATER && pc == FIRE) ||
+    (player == EARTH && pc == WATER)
   ) {
     console.log("GANASTE");
     conclusion = 1;
@@ -28,38 +31,57 @@ const play = (player) => {
     conclusion = -1;
   }
 
-  if (pc == PIEDRA) {
-    pcPiedra.classList.add("seleccionPc");
-    pcPapel.classList.remove("seleccionPc");
-    pcTijera.classList.remove("seleccionPc");
-    console.log("PC eligio Piedra ", PIEDRA);
+  if (pc == FIRE) {
+    pcFire.classList.add("seleccionPc");
+    pcWater.classList.remove("seleccionPc");
+    pcEarth.classList.remove("seleccionPc");
+    console.log("PC eligio Piedra ", FIRE);
     setTimeout(() => {
-      pcPiedra.classList.remove("seleccionPc");
-    }, 1000 * 3);
-  } else if (pc == PAPEL) {
-    pcPiedra.classList.remove("seleccionPc");
-    pcPapel.classList.add("seleccionPc");
-    pcTijera.classList.remove("seleccionPc");
-    console.log("PC eligio Papel ", PAPEL);
+      pcFire.classList.remove("seleccionPc");
+    }, 1000 * 1);
+  } else if (pc == WATER) {
+    pcFire.classList.remove("seleccionPc");
+    pcWater.classList.add("seleccionPc");
+    pcEarth.classList.remove("seleccionPc");
+    console.log("PC eligio Papel ", WATER);
     setTimeout(() => {
-      pcPapel.classList.remove("seleccionPc");
-    }, 1000 * 3);
+      pcWater.classList.remove("seleccionPc");
+    }, 1000 * 1);
   } else {
-    pcPiedra.classList.remove("seleccionPc");
-    pcPapel.classList.remove("seleccionPc");
-    pcTijera.classList.add("seleccionPc");
-    console.log("PC eligio Tijera ", TIJERA);
+    pcFire.classList.remove("seleccionPc");
+    pcWater.classList.remove("seleccionPc");
+    pcEarth.classList.add("seleccionPc");
+    console.log("PC eligio Tijera ", EARTH);
     setTimeout(() => {
-      pcTijera.classList.remove("seleccionPc");
-    }, 1000 * 3);
+      pcEarth.classList.remove("seleccionPc");
+    }, 1000 * 1);
   }
 };
 
+function playerWin(puntosPlayer, puntosPc) {
+  if (puntosPlayer + 1 == 5) {
+    document.querySelector(".modalSnake").classList.remove("d-none");
+    document.querySelector(".modalSnake-center p").textContent = "YOU WIN";
+    scorePc.textContent = "0";
+    scorePlayer.textContent = "0";
+    setTimeout(() => {
+      document.querySelector(".modalSnake").classList.add("d-none");
+    }, 1000 * 5);
+  } else if (puntosPc + 1 == 5) {
+    document.querySelector(".modalSnake").classList.remove("d-none");
+    document.querySelector(".modalSnake-center p").textContent = "YOU LOST";
+    scorePc.textContent = "0";
+    scorePlayer.textContent = "0";
+    setTimeout(() => {
+      document.querySelector(".modalSnake").classList.add("d-none");
+    }, 1000 * 5);
+  }
+}
+
 const score = (conclusion) => {
-  const scorePc = document.querySelector(".score-pc");
-  const scorePlayer = document.querySelector(".score-player");
   let valorPlayer = parseInt(scorePlayer.textContent);
   let valorPc = parseInt(scorePc.textContent);
+  playerWin(valorPlayer, valorPc);
 
   conclusion == 1
     ? (scorePlayer.textContent = valorPlayer + conclusion)
@@ -70,30 +92,34 @@ document.addEventListener("click", (e) => {
   if (e.target.matches("#cardPlayer-piedra")) {
     const piedra = document.querySelector(".cardPlayer-piedra");
     piedra.classList.toggle("seleccionPlayer");
-    play(PIEDRA);
+    play(FIRE);
     score(conclusion);
     setTimeout(() => {
       piedra.classList.toggle("seleccionPlayer");
-    }, 1000 * 3);
+    }, 1000 * 1);
   }
 
   if (e.target.matches("#cardPlayer-papel")) {
     const papel = document.querySelector(".cardPlayer-papel");
     papel.classList.toggle("seleccionPlayer");
-    play(PAPEL);
+    play(WATER);
     score(conclusion);
     setTimeout(() => {
       papel.classList.toggle("seleccionPlayer");
-    }, 1000 * 3);
+    }, 1000 * 1);
   }
 
   if (e.target.matches("#cardPlayer-tijera")) {
     const tijera = document.querySelector(".cardPlayer-tijera");
     tijera.classList.toggle("seleccionPlayer");
-    play(TIJERA);
+    play(EARTH);
     score(conclusion);
     setTimeout(() => {
       tijera.classList.toggle("seleccionPlayer");
-    }, 1000 * 3);
+    }, 1000 * 1);
+  }
+
+  if (e.target.matches(".modalSnake")) {
+    console.log("diste click es modal");
   }
 });
